8d34bb5285f408928a45dad361c094d38c34a760
Kriyss <perso@kriyss.ninja>
2015-04-13 22:40:02 +0200

    feat(IA): add log

--- src/java/com/tamina/cow4/IA.java
+++ src/java/com/tamina/cow4/IA.java
@@ -1,6 +1,8 @@
 package tamina.cow4;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tamina.cow4.message.AuthenticateAbstractMessage;
 import tamina.cow4.message.AuthenticateResponse;
 
@@ -10,6 +12,8 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class IA {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IA.class);
 
     private static final String       EOF    = "#end#";
     private static final ObjectMapper MAPPER = new ObjectMapper();
@@ -21,23 +25,25 @@ public class IA {
 
     public IA(String address, int port) {
         try {
+            LOG.info("Connect to COW server : {}:{}", address, port);
             this.server     = new Socket(address, port);
             this.inStream   = server.getInputStream();
             this.outStream  = server.getOutputStream();
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Can't connect to COW server : {}", e.getMessage());
         }
     }
 
-    public IA connect(String name, String avatarUrl) {
+    public IA authenticate(String name, String avatarUrl) {
         try {
             AuthenticateAbstractMessage auth = new AuthenticateAbstractMessage(name, avatarUrl);
             outStream.write((MAPPER.writeValueAsString(auth) + EOF).getBytes());
             outStream.flush();
             connectId = MAPPER.readValue(inStream, AuthenticateResponse.class).getId();
             System.out.println("Connect with id : " + connectId);
+            LOG.info("Authenticate : with name : {} and id : {}", name, connectId);
         } catch (IOException e) {
-            System.out.println("Error : " + e.getMessage());
+            LOG.info("Can't authenticate : {}", e.getMessage());
             throw new RuntimeException(e);
         }
         return this;
@@ -48,7 +54,7 @@ public class IA {
             this.inStream.close();
             this.outStream.close();
             this.server.close();
-            System.out.println("Close server socket");
+            LOG.info("Close server");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
